925e6d564fcaa49fdbe39b9bf61acb358db21ea1
ABHINAV RAI <allenrai1530@gmail.com>
2021-09-15 14:43:42 +0530

    update

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
           <Route path="/login">
            <Login/>
           </Route>
-          <Route exact path="/">
+          <Route exact path="/home">
             <Header/>
             <Home/>
           </Route>
