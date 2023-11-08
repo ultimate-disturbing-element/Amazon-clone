89c0b0729a26d4383b3ffea52aeb3cc28dbeaa53
Abhinav <A15930@aakashleap.com>
2023-11-08 11:26:22 +0530

    Update Firebase SDK Config

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
  const firebaseConfig = {
-    apiKey: "AIzaSyCcbbxOj7Zhrqv4pA-vycFNRG_CDgKC0Oo",
-    authDomain: "clone-971bc.firebaseapp.com",
-    projectId: "clone-971bc",
-    storageBucket: "clone-971bc.appspot.com",
-    messagingSenderId: "227106819133",
-    appId: "1:227106819133:web:633913f5044a3ae3d16e54"
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
   // Initialize Firebase
   const firebaseApp=firebase.initializeApp(firebaseConfig);
