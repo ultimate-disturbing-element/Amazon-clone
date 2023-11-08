b637a2d6973a5113de7d99a2f5f4d2191e92dab6
ABHINAV RAI <allenrai1530@gmail.com>
2023-11-08 21:32:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
           <Route path="/login">
            <Login/>
           </Route>
-          <Route exact path="/home">
+          <Route exact path="/Amazon-clone">
             <Header/>
             <Home/>
           </Route>
